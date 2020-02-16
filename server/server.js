8f25db5ef912df030d96f5993b3e0d1f2dc51d6a
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-02-16 13:39:17 -0800

    json code added

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(function(err, req, res, next) {
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds139523.mlab.com:39523/heroku_9gz062g9");
 
 // Start the API server
 app.listen(PORT, function() {
