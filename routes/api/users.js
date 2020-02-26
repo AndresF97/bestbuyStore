bdabbad26fae4ea103ff5b6e88d4bae095b286ac
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-02-26 11:20:29 -0800

    trying to deploy to heroku 2

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const isAuthenticated = require("../../config/isAuthenticated");
-const auth = require("../../server/config/auth");
+const auth = require("../../config/auth");
 const usersController = require("../../controllers/usersController");
 
 // LOGIN ROUTE
